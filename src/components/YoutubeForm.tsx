112731c4aeaf0d730c19e1b003884c3fd99c44d1
bookhabit <qnrgoqlt@naver.com>
2023-04-14 15:28:49 +0900

    react devtool과 기존 useState와 리렌더링 비교

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,18 +1,56 @@
-import React from 'react';
+import { useForm } from "react-hook-form";
+import {DevTool} from "@hookform/devtools"
+import { useState } from "react";
+
+let renderCount = 0
 
 export const YoutubeForm = () => {
+    const form = useForm();
+    const {register,control} = form
+    // 기존 방식
+    const [inputs, setInputs] = useState({
+        username2: '',
+        email2: '',
+        channel2:''
+      });
+    
+      const { username2, email2,channel2 } = inputs; // 비구조화 할당을 통해 값 추출
+    
+      const onChange = (e:any) => {
+        const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+        setInputs({
+          ...inputs, // 기존의 input 객체를 복사한 뒤
+          [name]: value // name 키를 가진 값을 value 로 설정
+        });
+      };
+
+    renderCount++
+
     return (
-        <form>
-            <label htmlFor='username'>Username</label>
-            <input type='text' id='username' name='username'/>
+        <>
+            <h1>Yotube Form ({renderCount/2})</h1>
+            <form>
+                <label htmlFor='username'>Username</label>
+                <input type='text' id='username' {...register("username")} />
 
-            <label htmlFor='email'>E-mail</label>
-            <input type='email' id='email' name='email'/>
+                <label htmlFor='email'>E-mail</label>
+                <input type='email' id='email' {...register("email")}/>
 
-            <label htmlFor='channel'>Channel</label>
-            <input type='text' id='channel' name='channel'/>
+                <label htmlFor='channel'>Channel</label>
+                <input type='text' id='channel' {...register("channel")}/>
 
-            <button>Submit</button>
-        </form>
+                <label htmlFor='username2'>Username2</label>
+                <input type='text' name='username2' onChange={onChange} value={username2} />
+
+                <label htmlFor='email2'>E-mail</label>
+                <input type='email' name='email2' onChange={onChange} value={email2} />
+
+                <label htmlFor='channel2'>Channel2</label>
+                <input type='text' name='channel2' onChange={onChange} value={channel2} />
+
+                <button>Submit</button>
+                <DevTool control={control} />
+            </form>
+        </>
     );
 };
